3159cf3a2597dcbbf20da9d3f6d661569ff91f2d
Mihir Paradkar <mkp65@gmail.com>
2016-04-28 15:26:26 -0400

    cleaned up and ready for submission

--- datasets/Carbon Emissions from Cars | American Forests_files/300lo.js
+++
@@ -1,1 +0,0 @@
-addthis.cbs.oln9_43798044564190250({"loc":"MTQ4NTBOQVVTTlkyMjI0MTAzNTU1NTAwMDAwVg==","urls":[],"subscription":{"active":true,"edition":"BASIC","tier":"basic","reducedBranding":false},"pc":"wpp","customMessageTemplates":[]});
--- datasets/Carbon Emissions from Cars | American Forests_files/addthis.js
+++
@@ -1,9 +0,0 @@
-    var addthis_config = {
-        ui_open_windows: true
-    };
-    var addthis_share = {
-        templates: {
-		tw:'via="YOUR USERNAME"'
-        }
-    }
-
